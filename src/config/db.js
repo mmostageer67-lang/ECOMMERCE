00f7bc27fb8048897a9d9e68c83bb01c5c4fc058
mahmoud mostageer <mmostageer67@gmail.com>
2026-06-17 12:29:12 +0300

    connectiong with mongo db

---
+++ src/config/db.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+async function connectDB() {
+  const uri = process.env.PROD_MONGODB_URI;
+  if (!uri) {
+    throw new Error('MONGODB_URI not defined');
+  }
+
+  await mongoose.connect(uri);
+}
+
+mongoose.connection.on('connected', () => {
+  console.log(`MongoDB connected: ${mongoose.connection.host}`);
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error('MongoDB connection error:', err);
+  process.exit(1);
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.warn('MongoDB disconnected');
+});
+
+
+module.exports = connectDB;
