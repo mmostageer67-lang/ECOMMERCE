c7431b36e227f87995fbfb2ce682df7d45afdbc3
mahmoud mostageer <mmostageer67@gmail.com>
2026-06-29 20:18:22 +0300

    rate limiter

--- src/app.js
+++ src/app.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const morgan = require('morgan');
-
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 const app = express();
 
+const limiter = rateLimit({
+    max: 5,
+
+    windowMs: 15 * 60 * 1000, 
+  message: 'Too many requests from this IP, please try again after 15 minutes',
+});
+
 app.use(express.json());
+app.use(helmet());
+app.use(limiter);
+
 app.use(morgan('dev'));
 
 
