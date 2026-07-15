092196f4720788923a05cfbb0430d5838e304d93
mahmoud mostageer <mmostageer67@gmail.com>
2026-07-15 05:36:00 +0300

    create register user service

---
+++ src/modules/auth/auth.service.js
@@ -0,0 +1,24 @@
+const User = require('../user/user.model');
+
+const register = async (userData) => {
+  const { name, email, password, role, phone, address } = userData;
+
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    throw new Error('Email already exists');
+  }
+
+ 
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role,
+    phone,
+    address,
+  });
+
+return user;
+};
+
+module.exports = { register };
