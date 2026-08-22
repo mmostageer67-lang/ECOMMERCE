c9b9737d26712200e14c754b2eb7c67445fb11f8
mahmoud mostageer <mmostageer67@gmail.com>
2026-08-22 20:55:23 +0300

    hashing password

--- src/modules/user/user.model.js
+++ src/modules/user/user.model.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
-const SALT_ROUNDS = 10;
 
 const userSchema = new mongoose.Schema(
   {
@@ -52,7 +51,7 @@ const userSchema = new mongoose.Schema(
 
 userSchema.pre('save', async function () {
   if (!this.isModified('password')) return;
-  this.password = await bcrypt.hash(this.password, SALT_ROUNDS);
+  this.password = await bcrypt.hash(this.password,12);
 });
 
 
